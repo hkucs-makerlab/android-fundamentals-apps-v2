308ce6cf024aca7c064bdb974b6af9a6afd4906f
hkucs-makerlab <makerlab@cs.hku.hk>
2019-08-06 10:59:53 +0800

    added 99.0-custom-button-with-effect, 99.1-custom-button-with-effect

---
+++ 99.0-custom-button-with-effect/app/src/main/java/com/example/custombutton/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.custombutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+/*
+    https://codinginflow.com/tutorials/android/custom-button-with-images
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button customButton = findViewById(R.id.custom_button);
+        Switch switchEnableButton = findViewById(R.id.switch_enable_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switchEnableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    customButton.setEnabled(true);
+                } else {
+                    customButton.setEnabled(false);
+                }
+            }
+        });
+    }
+}
